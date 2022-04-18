83bfd1bdde463a63d4488c73702da01f158da9e5
reni_rei <rei100423@gmail.com>
2022-04-18 23:28:24 +0900

    師弟杯のやつ削除

--- shiteihai.tf
+++ shiteihai.tf
@@ -2,22 +2,22 @@
 # 師弟杯
 ##############################
 
-resource "discord_category_channel" "shiteihai_0417" {
-  name      = "4月17日師弟杯斡旋"
-  server_id = discord_server.my_server.id
-  position  = 6
-}
+# resource "discord_category_channel" "shiteihai_0417" {
+#   name      = "4月17日師弟杯斡旋"
+#   server_id = discord_server.my_server.id
+#   position  = 6
+# }
 
-resource "discord_text_channel" "sisho" {
-  name      = "師匠になれます！"
-  server_id = discord_server.my_server.id
-  category  = discord_category_channel.shiteihai_0417.id
-  position  = 37
-}
+# resource "discord_text_channel" "sisho" {
+#   name      = "師匠になれます！"
+#   server_id = discord_server.my_server.id
+#   category  = discord_category_channel.shiteihai_0417.id
+#   position  = 37
+# }
 
-resource "discord_text_channel" "deshi" {
-  name      = "弟子にさせてください！"
-  server_id = discord_server.my_server.id
-  category  = discord_category_channel.shiteihai_0417.id
-  position  = 38
-}
+# resource "discord_text_channel" "deshi" {
+#   name      = "弟子にさせてください！"
+#   server_id = discord_server.my_server.id
+#   category  = discord_category_channel.shiteihai_0417.id
+#   position  = 38
+# }
